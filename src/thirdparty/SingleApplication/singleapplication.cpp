77416439119642fd399a2f999b58edcab25b92b6
Emeric <emeric.grange@gmail.com>
2020-04-06 18:41:31 +0200

    qDebug is enough

--- src/thirdparty/SingleApplication/singleapplication.cpp
+++ src/thirdparty/SingleApplication/singleapplication.cpp
@@ -51,7 +51,7 @@ SingleApplication::SingleApplication( int &argc, char *argv[], bool allowSeconda
 #if defined(Q_OS_ANDROID) || defined(Q_OS_IOS)
     // On Android and iOS since the library is not supported fallback to
     // standard QApplication behaviour by simply returning at this point.
-    qWarning() << "SingleApplication is not supported on Android and iOS systems.";
+    qDebug() << "SingleApplication is not supported on Android and iOS systems.";
     return;
 #endif
 
