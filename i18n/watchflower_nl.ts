31b5017fa77d4e99b92382c616633b1557743fec
FYr76 <63539322+FYr76@users.noreply.github.com>
2020-04-13 00:50:26 +0200

    Update watchflower_nl.ts

--- i18n/watchflower_nl.ts
+++ i18n/watchflower_nl.ts
@@ -56,7 +56,7 @@
     <message>
         <location filename="../qml/About.qml" line="409"/>
         <source>A plant monitoring application for Xiaomi / MiJia &apos;Flower Care&apos; and &apos;Ropot&apos; Bluetooth sensors and thermometers.</source>
-        <translation type="unfinished"></translation>
+        <translation>Een plantobservatieapp voor Xiaomi / MiJia &apos;Flower Care&apos; en &apos;Ropot&apos; Bluetooth sensors en thermometers.</translation>
     </message>
 </context>
 <context>
